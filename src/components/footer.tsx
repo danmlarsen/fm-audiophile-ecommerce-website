8a467721830ef99f8adaf34843ece310d49a7385
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-04-28 16:34:37 +0200

    Added socials

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
-import Navigation from './navigation';
-import Logo from './ui/logo';
+import Navigation from "./navigation";
+import Socials from "./socials";
+import Logo from "./ui/logo";
 
 export default function Footer() {
   return (
@@ -8,13 +9,17 @@ export default function Footer() {
         <Logo />
         <Navigation />
       </div>
-      <div className="grid grid-cols-2 mt-9 gap-y-14">
+      <div className="mt-9 grid grid-cols-2 gap-y-14">
         <p>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you
-          get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
+          Audiophile is an all in one stop to fulfill your audio needs. We're a
+          small team of music lovers and sound specialists who are devoted to
+          helping you get the most out of personal audio. Come and visit our
+          demo facility - we’re open 7 days a week.
         </p>
-        <div className="flex justify-end items-center row-span-2">socials</div>
-        <small>Copyright 2021. All Rights Reserved</small>
+        <div className="row-span-2 flex items-center justify-end">
+          <Socials />
+        </div>
+        <small>Copyright 2025. All Rights Reserved</small>
       </div>
     </footer>
   );
